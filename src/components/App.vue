91a67394d39d832905567a6196cd7460c67d6626
Matt Schlosser <hello@mattschlosser.me>
2020-10-31 17:01:37 -0600

    Added basic chart

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,10 +1,11 @@
-<template>
-    <div>
-        Hello World
-    </div>
-</template>
 <script>
+import { Bar } from 'vue-chartjs'
+import data from '../../local/20200531.json'
+console.log(data);
 export default {
-    
+  extends: Bar,
+  mounted () {
+    this.renderChart(data, {})
+  }
 }
 </script>
