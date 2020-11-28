79ef86d9b46198b7516c54bfa5ef424b6637cb3d
Matt Schlosser <hello@mattschlosser.me>
2020-11-28 15:53:09 -0700

    Fix data processor

--- ageProcessor.js
+++ ageProcessor.js
@@ -3,7 +3,6 @@ const path = require('path');
 const {parse, TextNode} = require('node-html-parser')
 
 let dir2 = fs.readdirSync(path.join(__dirname, "pages"));
-console.log(dir2);
 for (let filename of dir2) {
     if (filename.match(/\.htm/)) {
         let data = fs.readFileSync(path.join(__dirname, "pages", filename));
@@ -15,7 +14,6 @@ for (let filename of dir2) {
         }
         let d = new Date(date);
         let myDate = `${("" + d.getFullYear()).slice(-2)}${("0" + (d.getMonth() + 1)).slice(-2)}${("0" + d.getDate()).slice(-2)}`;
-        console.log(myDate);
         if (!fs.existsSync(`age/20${myDate}.json`)) {
             if (myDate > 200409) {
                 // process.exit(0);
@@ -38,11 +36,11 @@ for (let filename of dir2) {
                         stats.push({
                             category: rags[i][0], 
                             male_cases: rags[i][3], 
-                            male_deaths: rags[i][4], 
-                            femake_cases: rags[i][5], 
-                            female_deaths: rags[i][6], 
+                            male_percent: rags[i][4], 
+                            female_cases: rags[i][5], 
+                            female_percent: rags[i][6], 
                             unknown_cases: rags[i][7], 
-                            unknown_deaths: rags[i][8], 
+                            unknown_percent: rags[i][8], 
                             all_cases: rags[i][9], 
                             percent: rags[i][10]
                         })
@@ -63,11 +61,11 @@ for (let filename of dir2) {
                         stats.push({
                             category: rags[i][0], 
                             male_cases: rags[i][1], 
-                            male_deaths: rags[i][2], 
-                            femake_cases: rags[i][3], 
-                            female_deaths: rags[i][4], 
+                            male_percent: rags[i][2], 
+                            female_cases: rags[i][3], 
+                            female_percent: rags[i][4], 
                             unknown_cases: 0, 
-                            unknown_deaths: 0, 
+                            unknown_percent: 0, 
                             all_cases: rags[i][5], 
                             percent: rags[i][6]
                         })
@@ -78,23 +76,6 @@ for (let filename of dir2) {
                 fs.writeFileSync(`age/20${myDate}.json`, deets);
 
             }
-            // process.exit(1);
-            // console.log(nodes0;)
-            // console.log(nodes)
-            // for (let script of scripts) {
-            //     let node = script.childNodes[0]
-            //     if (node instanceof TextNode) {
-            //         if (node.rawText.match(/Local geograph/)) {no
-            //             let parsed = JSON.parse(node.rawText);
-            //             console.log("Found");
-            //             fs.writeFileSync(`${myDate}.json`, node.rawText);
-            //             // process.exit(0);
-            //         }
-            //     }
-            // }
-        } else {
-            // console.error("Not a valid date");
-            // process.exit(1);
         }
     }
 }
