3f93d3b19f4cc6f39bc325337c28266df13c1e02
Matt Schlosser <hello@mattschlosser.me>
2021-03-21 19:40:59 -0600

    Update for Sun 21 Mar 2021 07:40:59 PM MDT

--- src/components/DailyData.vue
+++ src/components/DailyData.vue
@@ -8,17 +8,30 @@
     />
 </template>
 <script>
-import allData from '../../data/dailyCaseCounts.json'
-allData.sort((a,b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
 import QuickChart from './Chart/QuickChart.vue'
 export default {
     components: {
         QuickChart
     }, 
+    async created() {
+        let allData = await  import("../../data/dailyCaseCounts.json").then(r=>r.default);
+        /* eslint-disable-next-line */
+        let allAllData = await import("../../data/allCaseCounts.json").then(r=>r.default);
+        console.log(allAllData);
+        allData.sort((a,b) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
+        allData.forEach((e) => {
+            e.data.forEach((e,i,a) => { 
+                if (!i) return;
+                e["net change"] = e["active cases"] - a[i-1]["active cases"]
+            });
+        })
+        this.allData = allData;
+    }, 
     data() {
         return {
-            allData,
+            allData: [],
             modes: [
+                "net change",
                 "active cases",
                 "current hospitalizations",
                 "current ICU",
