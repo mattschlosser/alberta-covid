7adbc3a999e378328f0907577d3996c964f1bb2c
Matt Schlosser <hello@mattschlosser.me>
2021-05-25 21:05:41 -0600

    Build

--- data-shaper.js
+++ data-shaper.js
@@ -42,4 +42,4 @@ shape_dir("severe", "category", ["all_cases", "hospitalized", "hospitalized_pct"
 shape_dir("municipal", "name", ["active", "cases", "recovered", "deaths"]);
 
 // ageVaccinatoins 
-shape_dir("ageVaccinations", "age_group", ["population", "dose_1", "dose_1_oct", "dose_2", "dose_2_pct", "total" ])
+shape_dir("ageVaccinations", "age_group", ["population", "dose_1", "dose_1_pct", "dose_2", "dose_2_pct", "total" ])
