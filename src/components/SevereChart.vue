13598692c7696ef250df677a511affb1867668e7
Matt Schlosser <hello@mattschlosser.me>
2021-06-06 15:36:32 -0600

    Update for Sun 06 Jun 2021 03:36:32 PM MDT

--- src/components/SevereChart.vue
+++ src/components/SevereChart.vue
@@ -4,7 +4,7 @@
         keyed-by="category"
         :modes="modes"
         :friendlyModes="friendlyModes"
-        init-category=0
+        :init-category="0"
         init-mode="deaths"
         
     >
@@ -22,26 +22,53 @@ export default {
     }, 
     data() {
         return {
-            allData,
+            allData: allData.map(f => {
+                f.data = f.data.map((e,i,a) => {
+                    if (!i) return e;
+                    e.new_hospitalized = e.hospitalized - a[i-1].hospitalized;
+                    e.new_icu = e.icu - a[i-1].icu;
+                    e.new_deaths = e.deaths - a[i-1].deaths;
+                    return e;
+                })
+                return f;
+            }),
             modes: [
                 "hospitalized",
+                {
+                    name: "new_hospitalized",
+                    type: "bar", 
+                    backgroundColor: "#6666ff"
+                },
                 "hospitalized_pct",
                 "hospitalized_rate",
                 "icu",
+                {
+                    name: "new_icu",
+                    type: "bar", 
+                    backgroundColor: "#66ff66"
+                },
                 "icu_pct",
                 "icu_rate",
                 "deaths",
+                {
+                    name: "new_deaths",
+                    type: "bar", 
+                    backgroundColor: "#ff6666"
+                },
                 "deaths_pct",
                 "deaths_rate",
             ],
             friendlyModes: {
-                "hospitalized": "Hospitalized",
+                "hospitalized": "Total Hospitalized",
+                "new_hospitalized": "New Hospitalized",
                 "hospitalized_pct": "% of cases Hospitalized",
                 "hospitalized_rate": "Hospitalized per 100,000",
-                "icu": "ICU",
+                "icu": "Total ICU",
+                "new_icu": "New ICU Admissions",
                 "icu_pct": "% of cases admmitted to ICU",
                 "icu_rate": "ICU per 100,000",
-                "deaths": "Deaths",
+                "deaths": "Total Deaths",
+                "new_deaths": "New Deaths",
                 "deaths_pct": "% of cases resulting in death",
                 "deaths_rate": "Deaths per 100,000",
             }
