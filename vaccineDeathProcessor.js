797a3dbf9b666df18165462e1f2df883fd9490c3
Matt Schlosser <hello@mattschlosser.me>
2021-09-16 21:46:04 -0600

    clean up vaccine processor script

--- vaccineDeathProcessor.js
+++ vaccineDeathProcessor.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const {parse, TextNode} = require('node-html-parser');
-const { SSL_OP_NETSCAPE_CHALLENGE_BUG } = require('constants');
+const {parse} = require('node-html-parser');
 
 let files = fs.readdirSync(path.join(__dirname, "pages"));
 for (let filename of files) {
     if (filename.match(/\.htm/)) {
+        // stats were not provided beofre this time
         if (filename < '2021060317') {
             continue;
         }
-        console.log(filename);
         let data = fs.readFileSync(path.join(__dirname, "pages", filename));
         let file = parse(data);
         date = file.querySelector("#last-updated").childNodes[0].rawText;
@@ -17,15 +16,11 @@ for (let filename of files) {
         let myDate = `${("" + d.getFullYear()).slice(-2)}${("0" + (d.getMonth() + 1)).slice(-2)}${("0" + d.getDate()).slice(-2)}`;
         if (!fs.existsSync(`severeVaccine/20${myDate}.json`)) {
             let tables = file.querySelectorAll('#vaccine-outcomes ul');
-            // let hosptialized = tables[0]?.childNodes.filter(e => e.innerText.match(/hospitalized/));
-            // let deaths = tables[0]?.childNodes.filter(e => e.innerText.match(/COVID-19 deaths/));
-            // console.log(deaths?.[0]?.innerText);
-            // if (deaths) {
             let stats = [];
             for (let match of ['cases', 'hospitalized', 'deaths']) {
                 let row = tables[0]?.childNodes.filter(e => e.innerText.match(match));
-                // console.log(row[0].innerText);
                 let [_, unvaxed, total] = row[0]?.innerText.match(/\(([\d,]*)\/([\d,]*)\)/);
+                // remove commas from the numbers
                 unvaxed = unvaxed.split(',').join('');
                 total = total.split(',').join('');
                 stats.push({
@@ -35,21 +30,8 @@ for (let filename of files) {
                     total: Number(total)
                 })
             }
-            // let [_, hUnvaxed, hTotal] = hosptialized?.[0]?.innerText.match(/\((\d*)\/(\d*)\)/);
-
-            // let [_, unvaxed, total] = deaths?.[0]?.innerText.match(/\((\d*)\/(\d*)\)/);
-            //     // console.log(unvaxed, total);
-            // // }
-            // let stats = [{
-            //     category: 'deaths',
-            //     unvaxed: Number(unvaxed), 
-            //     vaxed: total-unvaxed,
-            //     total: Number(total)
-            // }]
-            console.log(stats);
             let deets = JSON.stringify(stats);
             fs.writeFileSync(`severeVaccine/20${myDate}.json`, deets);
-
         }
     }
 }
