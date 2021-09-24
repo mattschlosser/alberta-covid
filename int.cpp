acd7bd2bf2c391769e4e2c2dced6219e484184c2
Matt Schlosser <hello@mattschlosser.me>
2021-09-24 15:36:49 -0600

    Update for Fri 24 Sep 2021 03:36:49 PM MDT

---
+++ int.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <cmath>
+using namespace std;
+int main() {
+  printf("%f\n",round(10.5));
+  printf("%f\n",round(-10.5));
+  return 0;
+}
---
+++ round.c
@@ -0,0 +1,1 @@
+#include <stdio.h>\n#include <math.h>\nint main() { printf(%dn, round(10.5)); return 0; }
