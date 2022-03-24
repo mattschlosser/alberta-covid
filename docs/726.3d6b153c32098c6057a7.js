5a5d398b7891eb25df85d7398232a49beddc5e56
Matt Schlosser <hello@mattschlosser.me>
2022-03-24 15:51:38 -0600

    Update for Thu 24 Mar 2022 03:51:38 PM MDT

--- docs/726.3d6b153c32098c6057a7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkalberta_covid_analysis=self.webpackChunkalberta_covid_analysis||[]).push([[726],{7179:a=>{"use strict";a.exports=JSON.parse('{"Delta":{"name":"Delta","data":[{"x":"2021-10-26","y":1,"total":318},{"x":"2022-03-16","y":1,"total":1}]}}')}}]);
